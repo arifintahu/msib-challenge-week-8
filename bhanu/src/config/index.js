a02c7ea29536cac85dbb576cd967605672ee94d9
bhanurz <aswanggab@gmail.com>
2023-10-06 15:20:26 +0700

    feat: menambahkan fitur getLogs dan getLogsById

---
+++ bhanu/src/config/index.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+
+const config = {
+  PORT: parseInt(process.env.PORT) || 3000,
+  API: process.env.API || "api/v1",
+  OMDB_KEY: process.env.OMDB_KEY || "faf7e5bb",
+  OMDB_API: process.env.OMDB_API || "http://www.omdbapi.com",
+};
+
+module.exports = config;
