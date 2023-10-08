d40ad83b3573854bde32eb58358ff7c2910b0bde
Junatius <cjuniar0@gmail.com>
2023-10-08 22:09:32 +0800

    feat: add the logControllers

---
+++ Juna/src/controllers/index.js
@@ -0,0 +1,9 @@
+const movieControllers = require("./movie.controllers");
+const logControllers = require('./logControllers')
+
+// TODO: Add controller logControllers
+
+module.exports = {
+  movieControllers,
+  logControllers
+};
